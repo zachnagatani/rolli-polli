8ddfb411c34145a6731b6d6fab885a7da88ad818
Zachariah Nagatani <zachnagatani@gmail.com>
2017-02-14 17:02:23 -0800

    Feat: User serial/deserialization

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
 const express = require('express'),
-      app = express()
+      app = express(),
+      session = require('express-session'),
       mongo = require('mongodb').MongoClient,
       mongoose = require('mongoose'),
       dbURL = require('./config/dbInfo')(),
+      secret = require('./config/secret'),
+      passport = require('passport'),
+      initPassport = require('./auth/init'),
+      bodyParser = require('body-parser'),
       port = process.env.PORT || 8000;
 
 mongoose.connect(dbURL);
+
+app.use(bodyParser());
+app.use(session({ secret: secret() }));
+app.use(passport.initialize());
+app.use(passport.session());
+initPassport(passport);
 
 app.get('/', (req, res) => {
     res.end('HELLO BABALOO');
---
+++ server/auth/init.js
@@ -0,0 +1,13 @@
+const passport = require('passport');
+
+module.exports = (passport) => {
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+            done(err, user);
+        });
+    });
+};
