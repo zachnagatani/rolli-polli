5086ba771670fab039640da26d375bf59aa245f4
Zachariah Nagatani <zachnagatani@gmail.com>
2017-02-15 21:22:20 -0800

    FeaT: Angular Hello World

--- client/app.js
+++ client/app.js
@@ -1,3 +1,11 @@
 (() => {
     'use strict';
+
+    const app = angular.module('rolliPolli', []);
+
+    app.controller('mainCtrl', function() {
+        const self = this;
+
+        this.name = "Rolli Polli";
+    });
 })();
