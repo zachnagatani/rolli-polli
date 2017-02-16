e355140ff029d627272f561b507106834114c22e
Zachariah Nagatani <zachnagatani@gmail.com>
2017-02-16 09:51:44 -0800

    Feat: Home Page

--- bs-config.js
+++ bs-config.js
@@ -19,7 +19,7 @@ module.exports = {
             "port": 8080
         }
     },
-    "files": "**/*",
+    "files": ["**/*.js", "**/*.css", "**/*.html"],
     "watchEvents": [
         "change"
     ],
--- client/app.js
+++ client/app.js
@@ -1,7 +1,19 @@
 (() => {
     'use strict';
 
-    const app = angular.module('rolliPolli', []);
+    const app = angular.module('rolliPolli', ['ui.router']);
+
+    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home',  {
+                url: '/home',
+                templateUrl: 'home/home.html',
+                controller: 'homeCtrl as vm'
+            });
+
+        $urlRouterProvider
+            .otherwise('/home');
+    }]);
 
     app.controller('mainCtrl', function() {
         const self = this;
