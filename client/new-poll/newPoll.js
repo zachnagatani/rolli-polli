f047c342c09dda769876e79aaedfd98c7c472296
Zachariah Nagatani <zachnagatani@gmail.com>
2017-02-20 22:10:28 -0800

    Feat: Basic Poll Creation Functionality

--- client/new-poll/newPoll.js
+++ client/new-poll/newPoll.js
@@ -2,11 +2,34 @@
     'use strict';
 
     angular.module('rolliPolli')
-        .controller('newPollCtrl', ['api', function(api) {
+        .controller('newPollCtrl', ['api', 'auth', function(api, auth) {
             const self = this;
 
             self.getNumber = num => {
                 return new Array(num);
+            };
+
+            self.postPoll = (question, optionsNumber, e) => {
+                e.preventDefault();
+                const form = document.getElementById('poll-form');
+
+                let counter = 1,
+                    options = [];
+                while (counter <= optionsNumber) {
+                    console.log(form.elements['option' + counter].value);
+                    options.push({name: form.elements['option' + counter].value});
+                    counter++;
+                }
+
+                api.post('http://localhost:8000/new-poll', {
+                    username: auth.currentUser().username,
+                    question: question,
+                    options: options
+                }).then(function sucess(response) {
+                    console.log(response.data);
+                }, function error(err) {
+                    console.log(err);
+                });
             };
         }]);
 })();
